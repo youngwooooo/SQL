90c347a63c4cb2249bf741741f9d96c1abb8da6a
DESKTOP-9R4PM6M\PC-25 <dlddudn02@gmail.com>
2021-03-11 12:51:01 +0900

    SELECT 쿼리 기본 문법 실습

---
+++ 2021_03_11.sql
@@ -0,0 +1,13 @@
+-- 데이터 조회 방법
+-- FORM: 데이터를 조회할 테이블 명시
+-- SELECT: 조회하고자 하는 컬럼명(테이블에 존재하는 컬럼명)
+--         테이블의 모든 컬럼을 조회 할 경우 *를 기술
+
+SELECT empno,ename
+FROM EMP;
+
+SELECT *
+FROM EMP;
+-- EMPNO: 직원번호, ENAME: 직원이름, JOB: 담당업무
+-- MGR: 상위 담당자, HIREDATE: 입사일자, SAL: 급여
+-- COMM: 상여금, DEPTNO: 부서번호
